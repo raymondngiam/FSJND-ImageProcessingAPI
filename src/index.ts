f3a28f57340fc4a4559b82302c4236e86fff883f
raymondngiam <ha.ngiam87@gmail.com>
2022-12-05 17:06:08 +0800

    chore: setup eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,17 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send('Hello world!');
+app.get('/', (req, res) => {
+  res.send('Hello world!');
 });
 
-app.listen(port,()=>{
-    console.log(`Server started at http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
 });
 
 // for dummy test
-function add(a: number, b: number):number{ return a + b };
+const add = (a: number, b: number): number => {
+  return a + b;
+};
 
-export default {add};
+export default { add };
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,7 +1,7 @@
-import _ from "../index";
+import _ from '../index';
 
 // dummy test
-it("should add two numbers", async () => {
-  const data = _.add(3,4);
+it('should add two numbers', async () => {
+  const data = _.add(3, 4);
   expect(data).toEqual(7);
 });
