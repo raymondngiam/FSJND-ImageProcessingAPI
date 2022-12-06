88cf470189057284666a471b48e9e9d30ea07b66
raymondngiam <ha.ngiam87@gmail.com>
2022-12-06 17:41:36 +0800

    feat: add resize and shape function in imgproc module

---
+++ src/modules/imgproc.ts
@@ -0,0 +1,34 @@
+import sharp from 'sharp';
+
+async function resize(
+  inputPath: string,
+  outputPath: string,
+  width: number,
+  height: number
+): Promise<string | null> {
+  try {
+    await sharp(inputPath).resize(width, height).toFile(outputPath);
+    return outputPath;
+  } catch (error) {
+    console.log(`Error: ${error}`);
+    return null;
+  }
+}
+
+async function shape(inputPath: string): Promise<[number, number] | null> {
+  try {
+    const meta = await sharp(inputPath).metadata();
+    const imgWidth = meta.width;
+    const imgHeight = meta.height;
+    if (imgWidth != undefined && imgHeight != undefined) {
+      return [imgWidth, imgHeight];
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.log(`Error: ${error}`);
+    return null;
+  }
+}
+
+export default { resize, shape };
